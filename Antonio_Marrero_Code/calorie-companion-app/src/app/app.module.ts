84ed42a6918d3d806a9646b0eeb0e48d2a0cb7a9
Antonio Marrero Bonilla <antonio.marrerobonilla@gmail.com>
2018-05-01 16:33:26 -0400

    Created login and registration components with no implementation yet.

--- Antonio_Marrero_Code/calorie-companion-app/src/app/app.module.ts
+++ Antonio_Marrero_Code/calorie-companion-app/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavComponent } from './components/nav/nav.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    DashboardComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
